25b0324a1e0300fdb8a9a9982fd956dad80e86f9
Nokz <dreamwithnokz@gmail.com>
2021-02-19 17:57:08 +0800

    Remove contact page

--- components/MenuNav.js
+++ components/MenuNav.js
@@ -7,46 +7,39 @@ const MenuNav = ({ imgSrc, onNavHover, onNavUnhover }) => {
   }
 
   return (
-    <>
-      <div className="menu-nav">
-        <div className="menu-nav__image">
-          <Image src={imgSrc} alt="My Avatar" width={200} height={200} />
-        </div>
-        <div className="menu-nav__navs d-flex">
-          <ul className="menu-nav__ul w-100 align-self-center">
-            <li className="menu-nav__li">
-              <Link href="/about">
-                <a
-                  className="menu-nav--effect"
-                  onMouseOver={handleNavHover}
-                  onMouseLeave={onNavUnhover}
-                  value="/menu-about.png"
-                >
-                  ABOUT
-                </a>
-              </Link>
-            </li>
-            <li className="menu-nav__li">
-              <Link href="/projects">
-                <a
-                  className="menu-nav--effect"
-                  onMouseOver={handleNavHover}
-                  onMouseLeave={onNavUnhover}
-                  value="/menu-projects.png"
-                >
-                  PROJECTS
-                </a>
-              </Link>
-            </li>
-            <li className="menu-nav__li">
-              <Link href="/contact">
-                <a className="menu-nav--effect">CONTACT</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
+    <div className="menu-nav">
+      <div className="menu-nav__image">
+        <Image src={imgSrc} alt="My Avatar" width={200} height={200} />
       </div>
-    </>
+      <div className="menu-nav__navs d-flex">
+        <ul className="menu-nav__ul w-100 align-self-center">
+          <li className="menu-nav__li">
+            <Link href="/about">
+              <a
+                className="menu-nav--effect"
+                onMouseOver={handleNavHover}
+                onMouseLeave={onNavUnhover}
+                value="/menu-about.png"
+              >
+                ABOUT
+              </a>
+            </Link>
+          </li>
+          <li className="menu-nav__li">
+            <Link href="/projects">
+              <a
+                className="menu-nav--effect"
+                onMouseOver={handleNavHover}
+                onMouseLeave={onNavUnhover}
+                value="/menu-projects.png"
+              >
+                PROJECTS
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
