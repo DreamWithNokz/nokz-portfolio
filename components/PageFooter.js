a5203df159f611f51d336312f5568c10b3a2c809
Nokz <dreamwithnokz@gmail.com>
2021-02-20 19:37:28 +0800

    Add link on page footer

--- components/PageFooter.js
+++ components/PageFooter.js
@@ -1,14 +1,28 @@
+import Link from "next/link";
 import { Container } from "react-bootstrap";
 import { AiFillTwitterCircle, AiFillGithub } from "react-icons/ai";
 
 const PageHeader = () => (
   <Container className="footer">
-    <span className="footer__copyright">&#169; 2020 - 2021 dreamwithnokz</span>
+    <span className="footer__copyright">
+      &#169; 2020 - 2021{" "}
+      <Link href="/">
+        <span className="footer__name">dreamwithnokz</span>
+      </Link>
+    </span>
     <span className="float-right footer__links">
-      <a href="https://twitter.com/DreamWithNokz" target="_blank" className="footer__link">
+      <a
+        href="https://twitter.com/DreamWithNokz"
+        target="_blank"
+        className="footer__link"
+      >
         <AiFillTwitterCircle />
       </a>
-      <a href="https://github.com/DreamWithNokz" target="_blank" className="footer__link">
+      <a
+        href="https://github.com/DreamWithNokz"
+        target="_blank"
+        className="footer__link"
+      >
         <AiFillGithub />
       </a>
     </span>
