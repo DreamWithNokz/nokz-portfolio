18c4b7ddd1472bca576ce53ac6f3a6f6cf51f29f
Nokz <dreamwithnokz@gmail.com>
2021-03-20 03:57:01 +0800

    Fix FOUC attempt

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,10 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="ja">
-        <Head />
+      <Html lang="en">
+        <Head>
+          <meta charSet="UTF-8" />
+        </Head>
         <body>
           + <script> </script>
           <Main />
